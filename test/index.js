ccebe38acf2022d49e0ef091ce6b239849bfc7a1
christopherbalz <christopherbalz@yahoo.com>
2017-04-15 19:48:58 -0700

    Formatting.

--- test/index.js
+++ test/index.js
@@ -5,12 +5,16 @@ const {getMailStore} = require('../src/get_mail_store');
 const {scrapeMail} = require('../src/scrape_mail');
 
 const mailStore = getMailStore( mailServers[ testConf.testServer ].access );
+
 try {
   scrapeMail(mailStore, false, handleFinished);
 } catch (err) {
   console.error('Test failed.');
   throw err;
 }
+
+// - - -
+
 
 function handleFinished(status) {
   if( status.err ) {
