5f948768c9082ec60c3c0b49cf3a75872645f806
christopherbalz <christopherbalz@yahoo.com>
2017-04-22 14:01:51 -0700

    Remove username, pwd values from conf file (but maintain option to specify in conf).

--- conf/mail_servers.conf.js
+++ conf/mail_servers.conf.js
@@ -7,10 +7,20 @@ const mailServers = {
       fqDomain: 'mail.kattare.com',
       port: 110,
       username: 'treelogic_admin',
-      pwd: 'thundercl0uD1',
+      pwd: '', // @to-do: Get this from db in a salted form.
     },
     extractTasks: {
-      billing: {  // Object properties (keys) are 'and'd, and Array elements inside the properties are 'or'd.
+      billing: {
+        /*-
+         * Config Doc:
+         *
+         * Object keys (keys: 'subject', 'from', 'to', 'text', and so forth) are either 'and'd or 'or'd depending
+         * on the 'conditional' object specified below (see the 'localTest' config).
+         * If it is left unspecified, then all keys are 'and'd.
+         * Also, if the 'conditional' object is specified, then only the relationships specified there are applied.
+         *
+         * Array elements inside the properties are 'or'd.  To use 'and' in that case, put the cases into a single regex.
+         */
         subject: [ /^Kattare:\s+Payment\s+Received\s*$/, /^[Ff]w[d]?:\s+Kattare:\s+Payment\s+Received\s*$/ ],
         from: [ /.*/ ], // Uses the 'address' field only, not name.
         to: [ /.*/ ],  // Uses the 'address' field only, not name.
@@ -32,6 +42,7 @@ const mailServers = {
         from: [ /.*/ ], // Uses the 'address' field only, not name.
         to: [ /.*/ ],  // Uses the 'address' field only, not name.
         text: { fixed_amount_bill: /Amount:\s*\$\s*(\d+)/gm, network_usage_bill: NO_MATCH },  // This is the e-mail body text.
+        conditional: [ { terms: ['subject', 'text'], operator: 'or' } ],
       }
     }
   }
