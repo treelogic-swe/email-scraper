63a43f1eb1ef9bbc182551e7d757b2a6e2ded757
christopherbalz <christopherbalz@yahoo.com>
2017-04-22 14:00:26 -0700

    Enforce datatype of pwd.

--- src/get_mail_store.js
+++ src/get_mail_store.js
@@ -6,5 +6,9 @@ function getMailStore(conf) {
   const {protocol, fqDomain, port, username, pwd} = conf;
   const mailStore = mailx.store(protocol, fqDomain, port, username, pwd); // @to-do: Pull this from salted database entry.
 
+  if( !pwd ) {
+    pwd = '';
+  }
+
   return mailStore;
 }
