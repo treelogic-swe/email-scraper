41669a8afa6f7055e87159c7086610bcd0dfd4b2
christopherbalz <christopherbalz@yahoo.com>
2017-04-22 12:27:56 -0700

    Use a local mailserver for the tests.  Eliminates dependency on external mailserver.

--- test/index.js
+++ test/index.js
@@ -4,14 +4,19 @@ const {mailServers} = require('../conf/mail_servers.conf');
 const {getMailStore} = require('../src/get_mail_store');
 const {scrapeMail} = require('../src/scrape_mail');
 
-const scrapeConf = mailServers[ testConf.testServer ];
+const chalk = require('chalk');
 
+const passed = chalk.bgGreen.bold.white;
+const failed = chalk.bgRed.bold.white;
+
+const scrapeConf = mailServers[ testConf.testServer ];
 const mailStore = getMailStore( scrapeConf.access );
+
 
 try {
   scrapeMail( mailStore, scrapeConf.extractTasks, {listenForever: false, callback: handleFinished} );
 } catch (err) {
-  handleTestFailed(err);
+  handleTestFailed(err, 'Code or syntax error.');
 }
 
 // - - -
@@ -19,17 +24,25 @@ try {
 
 function handleFinished(status) {
   if( status.err ) {
-    handleTestFailed(status.err);
-  } else {
-    console.info('Test passed.');
+    handleTestFailed(status.err, 'Logic error.');
+  } else { // Test Assertions go here:
+    if( status.scrapeResultStatus.received === 1 ) {
+     console.info(passed('Test passed.') + chalk.green.bold(' \u2713  '));
+    } else {
+      handleTestFailed(null, 'Unexpected result: ' + JSON.stringify(status.scrapeResultStatus) + '; expected received 1.');
+    }
   }
 }
 
-function handleTestFailed(err) {
-  console.error('Test failed.');
+function handleTestFailed(err, reason) {
+  logFailed(reason);
   if(err) {
     console.info('The error message is:');
     console.log(err);
   }
   process.exit(1);
+}
+
+function logFailed(reason) {
+  console.error(failed('Test failed.  Reason: ' + reason) + chalk.red.bold(' \u274c  '));
 }
