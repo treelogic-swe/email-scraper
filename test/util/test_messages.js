d78fecaefb5edc1bc0dfe55928c9eda3aed835eb
christopherbalz <christopherbalz@yahoo.com>
2017-05-06 10:05:30 -0700

    Add third email message.

--- test/util/test_messages.js
+++ test/util/test_messages.js
@@ -8,9 +8,12 @@ const messages = [
   'FOO Tere ÕÜÄÖŠ!\n------------------\n\nKell on praegu **' + curtime + '**\n\nVaata ka:\nAmount: $100.00\n\n  * [Delfi](http://www.delfi.ee)\n  * [NETI]' +
       '(http://www.neti.ee)\n  * [EPL](http://www.epl.ee)\n\n*Koodiblokk*\n\n    for(va' +
       'r i=0;i<100;i++){\n        alert(i+5);\n    }\n\n\nParimat,  \nKellamees',
-  'BAR Tere ÕÜÄÖŠ!\n------------------\n\nKell on praegu **' + curtime + '**\n\nVaata ka:\nAmount: $100.00\n\n  * [Delfi](http://www.delfi.ee)\n  * [NETI]' +
+  'BAR Tere ÕÜÄÖŠ!\n------------------\n\nKell on praegu **' + curtime + '**\n\nVaata ka:\nAmount: $110.00\n\n  * [Delfi](http://www.delfi.ee)\n  * [NETI]' +
       '(http://www.neti.ee)\n  * [EPL](http://www.epl.ee)\n\n*Koodiblokk*\n\n    for(va' +
-      'r i=0;i<100;i++){\n        alert(i+5);\n    }\n\n\nParimat,  \nKellamees'
+      'r i=0;i<100;i++){\n        alert(i+5);\n    }\n\n\nParimat,  \nKellamees',
+  'BAT Tere ÕÜÄÖŠ!\n------------------\n\nKell on praegu **' + curtime + '**\n\nVaata ka:\nAmount: $100.00\n\n  * [Delfi](http://www.delfi.ee)\n  * [NETI]' +
+      '(http://www.neti.ee)\n  * [EPL](http://www.epl.ee)\n\n*Koodiblokk*\n\n    for(va' +
+      'r i=0;i<100;i++){\n        alert(i+5);\n    }\n\n\nParimat,  \nKellamees',
 ];
 
 const messageMetas = [
@@ -22,10 +25,16 @@ const messageMetas = [
     subject: 'Kattare: Payment Received'
   }, {
     toName: 'Foo Bar',
-    toAddress: 'bat.reinman@gmail.com',
+    toAddress: 'bar.reinman@gmail.com',
     fromName: 'baz Kämbu',
     fromAddress: 'foo.kambu@node.ee',
     subject: 'Kattare: Payment Failed'
+  }, {
+    toName: 'Bat Baz',
+    toAddress: 'bat.reinman@gmail.com',
+    fromName: 'baz Kämbu',
+    fromAddress: 'foo.kambu@node.ee',
+    subject: 'Kattare: Payment Received'
   }
 ];
 
