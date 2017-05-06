b0b0948732b268b4f0ba6a475782b8e872357c95
christopherbalz <christopherbalz@yahoo.com>
2017-05-06 10:15:39 -0700

    chore: format and sort lines.

--- src/command_line_options/command_line_option_definitions.js
+++ src/command_line_options/command_line_option_definitions.js
@@ -1,11 +1,11 @@
 const clOpts = [
-  { name: 'username', alias: 'u', type: String, defaultValue: 'test-user' },
-  { name: 'password', alias: 'p', type: String },
-  { name: 'mailserver', alias: 'm', type: String, defaultValue: 'localTest' },
-  { name: 'keepListening', alias: 'k', type: Boolean, defaultValue: false },
-  { name: 'startAt', alias: 's', type: Number, defaultValue: 0 },
-  { name: 'help', alias: 'h', type: Boolean, defaultValue: false },
-  { name: 'list', alias: 'l', type: Boolean, defaultValue: false },
+  { name: 'help',          alias: 'h', type: Boolean, defaultValue: false       },
+  { name: 'keepListening', alias: 'k', type: Boolean, defaultValue: false       },
+  { name: 'list',          alias: 'l', type: Boolean, defaultValue: false       },
+  { name: 'mailserver',    alias: 'm', type: String,  defaultValue: 'localTest' },
+  { name: 'password',      alias: 'p', type: String                             },
+  { name: 'startAt',       alias: 's', type: Number,  defaultValue: 0           },
+  { name: 'username',      alias: 'u', type: String,  defaultValue: 'test-user' },
 ];
 
 exports.commandLineOptionDefinitions = clOpts;
