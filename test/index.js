d58a0e3878250fa03fdce19a5ae5b4c9ada6cb15
christopherbalz <christopherbalz@yahoo.com>
2017-05-06 13:27:13 -0700

    Add second test case; clean up to isolate test assertions.

--- test/index.js
+++ test/index.js
@@ -9,34 +9,76 @@ const chalk = require('chalk');
 const passed = chalk.bgGreen.bold.white;
 const failed = chalk.bgRed.bold.white;
 
-const scrapeConf = mailServers[ testConf.testServer ];
-const mailStore = getMailStore( scrapeConf.access );
+const scrapeConf = mailServers[testConf.testServer];
 
+runTestSuite();
 
-try {
-  scrapeMail( mailStore, scrapeConf.extractTasks, {listenForever: false, callback: handleFinished} );
-} catch (err) {
-  handleTestFailed(err, 'Code or syntax error.');
+function runTestSuite() {
+  try {
+    let mailStore = setUp();
+    scrapeMail(mailStore, scrapeConf.extractTasks, {
+      listenForever: false,
+      callback: (status) => {                           // @to-do: Use async/await; need to use Promise in scraper.
+        handleFinished(status, handleReadAllTest);
+        mailStore = setUp();
+        scrapeMail(mailStore, scrapeConf.extractTasks, {
+          listenForever: false,
+          startAt: 1,
+          callback: (status) => {
+            handleFinished(status, handleReadFromSecondMailMessageTest);
+          }
+        });
+      }
+    });
+  } catch (err) {
+    handleTestFailed(err, 'Code or syntax error.');
+  }
 }
 
-// - - -
+// - - - Assertions
 
+function handleReadAllTest(status) {
+  processTestResult(status, '100,100');
+}
 
-function handleFinished(status) {
-  if( status.err ) {
+function handleReadFromSecondMailMessageTest(status) {
+  processTestResult(status, '100');
+}
+
+// - - - Utilities
+
+function setUp() {
+
+  return getMailStore(scrapeConf.access);
+}
+
+function tearDown(status) {
+  if (status && status.scrapeResultStatus) {
+    status.scrapeResultStatus.mailStatus = null;
+    status.scrapeResultStatus.scrapeResult = {};
+  }
+}
+
+function handleFinished(status, handleTestResult) {
+  if (status.err) {
     handleTestFailed(status.err, 'Logic error.');
   } else { // Test Assertions go here:
-    if( status.scrapeResultStatus.scrapeResult.billing.length == 2) {
-      console.info(passed('Test passed.') + chalk.green.bold(' \u2713  '));
-    } else {
-      handleTestFailed(null, 'Unexpected result: No matching data.');
-    }
+    handleTestResult(status);
+  }
+  tearDown(status);
+}
+
+function processTestResult(status, expected) {
+  if (status.scrapeResultStatus.scrapeResult.billing.join() === expected) {
+    console.info(passed('Test passed.') + chalk.green.bold(' \u2713  '));
+  } else {
+    handleTestFailed(null, 'Unexpected result: Expected this result: ' + expected);
   }
 }
 
 function handleTestFailed(err, reason) {
   logFailed(reason);
-  if(err) {
+  if (err) {
     console.info('The error message is:');
     console.log(err);
   }
