9ebab32170493fdc5e1439e7f87a19a88c4bc099
christopherbalz <christopherbalz@yahoo.com>
2017-05-20 13:28:27 -0700

    feat: add reserved date key.

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,10 @@ module.exports = {
   CONSOLE: 'console',
   CSV: 'csv',
   DATABASE: 'database',
+  DATE: 'email-scraper-reserved_date',
   FILE: 'file',
   FROM_EMAIL: 'from_email',
-  HASH: 'email-scraper-reserved-hash', // Used for writing to the database.
+  HASH: 'email-scraper-reserved_hash', // Used for writing to the database.
   HASH_COLUMN_NAME: 'hash',
   LAST_EMAIL_READ_DB_TABLE_NAME: 'last_email_read',
   LAST_EMAIL_READ_DB_TABLE_COLUMN_NAME: 'email_message_index',
