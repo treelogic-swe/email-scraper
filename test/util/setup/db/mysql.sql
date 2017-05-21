7eb96b66a2cc0796126b48d643cfa438882add11
christopherbalz <christopherbalz@yahoo.com>
2017-05-20 21:49:28 -0700

    Add a third column for email date.  Make the hash the primary key.

--- test/util/setup/db/mysql.sql
+++ test/util/setup/db/mysql.sql
@@ -1,6 +1,6 @@
 create database email_scraper_1;
 use email_scraper_1;
-create table billing ( hash VARCHAR(128), billing DECIMAL(7,2) );
+create table billing ( hash VARCHAR(128), date VARCHAR(128), billing DECIMAL(7,2), PRIMARY KEY (hash) );
 create table last_email_read ( email_message_index INT );
 CREATE USER 'foo'@'localhost' IDENTIFIED BY 'bar';
 GRANT ALL PRIVILEGES ON email_scraper_1.* TO 'foo'@'localhost';
